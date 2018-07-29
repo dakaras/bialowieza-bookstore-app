b788e5a0e76bf69772d22cd1ca48aeb67be17e6e
github username <github email address>
2018-07-28 19:13:26 -0500

    added authors/show.html.erb

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,2 +1,11 @@
 class AuthorsController < ApplicationController
+
+  def index
+    @authors = Author.all
+  end
+
+  def show
+    @author = Author.find_by(id: params[:id])
+    @books = @author.books 
+  end
 end
