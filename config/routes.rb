e32485b6114a0892aefed957c6a98cbb3b2b2a66
github username <github email address>
2018-07-31 11:24:29 -0500

    added shopping cart image to show pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/sessions', to: 'sessions#create'
-  post '/logout', to: 'sessions#destroy'
+  get '/logout', to: 'sessions#destroy'
 
   root 'site#index'
   get '/welcome', to:  'site#index'
